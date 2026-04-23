880fe8954499fb6f96043c4c1275de81e155d238
Balvant <balvant@m2webdesigning.com>
2026-04-23 10:20:13 +0530

    23-04-2026

--- app/routes/auth.login/route.jsx
+++ app/routes/auth.login/route.jsx
@@ -1,28 +1,18 @@
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { useState } from "react";
-import { Form, useActionData, useLoaderData } from "react-router";
+import { Form } from "react-router";
 import { login } from "../../shopify.server";
-import { loginErrorMessage } from "./error.server";
 
-export const loader = async ({ request }) => {
-  const errors = loginErrorMessage(await login(request));
-
-  return { errors };
+export const loader = async () => {
+  return null;
 };
 
 export const action = async ({ request }) => {
-  const errors = loginErrorMessage(await login(request));
-
-  return {
-    errors,
-  };
+  return login(request);
 };
 
 export default function Auth() {
-  const loaderData = useLoaderData();
-  const actionData = useActionData();
   const [shop, setShop] = useState("");
-  const { errors } = actionData || loaderData;
 
   return (
     <AppProvider embedded={false}>
@@ -36,7 +26,6 @@ export default function Auth() {
               value={shop}
               onChange={(e) => setShop(e.currentTarget.value)}
               autocomplete="on"
-              error={errors.shop}
             ></s-text-field>
             <s-button type="submit">Log in</s-button>
           </s-section>
